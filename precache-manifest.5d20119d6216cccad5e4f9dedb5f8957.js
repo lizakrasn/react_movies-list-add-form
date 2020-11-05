39878ac139df593b0369bac6b1c20092e0ae1e79
Liza Krasnoshchok <nlight1122@gmail.com>
2020-11-05 19:11:56 +0200

    Updates

--- precache-manifest.5d20119d6216cccad5e4f9dedb5f8957.js
+++ precache-manifest.5d20119d6216cccad5e4f9dedb5f8957.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18454cabe0e11589f3be91e83fde6f9f",
+    "revision": "96b67e25ff2cf917ba562acc5299e6b6",
     "url": "./index.html"
   },
   {
-    "revision": "c063cf03c13e6f2d793f",
+    "revision": "f1e791c4e1bab9dbf1fa",
     "url": "./static/css/2.67a0cd86.chunk.css"
   },
   {
-    "revision": "d7b226916d49d823f5ec",
+    "revision": "1da87c17f18ea138c6f0",
     "url": "./static/css/main.171b95df.chunk.css"
   },
   {
-    "revision": "c063cf03c13e6f2d793f",
-    "url": "./static/js/2.e51ade76.chunk.js"
+    "revision": "f1e791c4e1bab9dbf1fa",
+    "url": "./static/js/2.752375e1.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.e51ade76.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.752375e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d7b226916d49d823f5ec",
-    "url": "./static/js/main.557c0e36.chunk.js"
+    "revision": "1da87c17f18ea138c6f0",
+    "url": "./static/js/main.0804dbd8.chunk.js"
   },
   {
     "revision": "3cfb45b61c015a055433",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ca3884b6101adfe416bcd5811663f77.js"
+  "./precache-manifest.5d20119d6216cccad5e4f9dedb5f8957.js"
 );
 
 self.addEventListener('message', (event) => {
